1fce33dcd24891ccc24e139dbe1894a3cd9302ad
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2023-04-01 16:24:35 -0400

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,6 @@
-use std::sync::{Mutex, Arc};
-
+use std::sync::{Mutex, Arc, RwLock};
 use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
+use crate::tree::{Tree, Leaf, self};
 
     pub enum ErrorStates 
     {
@@ -8,14 +8,25 @@ use pickledb::{PickleDb, PickleDbDumpPolicy, SerializationMethod};
         DatabaseNonExistant
     } 
     pub struct State{
-       pub db: Arc<Mutex<PickleDb>>,
+       pub db: Arc<RwLock<PickleDb>>,
+       pub tree: Arc<RwLock<Tree>>
     }
 
     impl State {
 
         pub(crate) fn init() -> Self {
-            Self {db: Arc::new(Mutex::new(PickleDb::load("data.db", PickleDbDumpPolicy::AutoDump, SerializationMethod::Json).unwrap_or({
-            PickleDb::new("data.db", PickleDbDumpPolicy::AutoDump, SerializationMethod::Json)})))}
+            let database = PickleDb::load("data.db", PickleDbDumpPolicy::AutoDump, SerializationMethod::Json).unwrap_or({
+                PickleDb::new("data.db", PickleDbDumpPolicy::AutoDump, SerializationMethod::Json)});
+           
+            let tree: Tree = Tree::read_tree("/tree.json").unwrap_or({
+                // Todo: Get data from peer in p2p network , if empty => spawn() 
+                Tree::spawn()
+            });
+
+            Self {
+                db: Arc::new(RwLock::new(database)), 
+                tree: Arc::new(RwLock::new(tree)),                    
+            }
 
         }
 
